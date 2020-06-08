27ce9eccae37863fd9585e0f149f6ddebe4e9087
Manu Cornet <m@ma.nu>
2020-06-08 16:25:18 +0000

    Split out graphing functionality into a separate function.

--- js/healthmap.js
+++ js/healthmap.js
@@ -1,8 +1,5 @@
 // Constants
 const ANIMATION_FRAME_DURATION_MS = 300;
-const CASE_GRAPH_WIDTH_PX = 200;
-const CASE_GRAPH_HEIGHT_PX = 120;
-
 const COLOR_MAP = [
   ['#67009e', '< 10', 10],
   ['#921694', '11–100', 100],
@@ -135,61 +132,6 @@ function showLegend() {
   }
 }
 
-function sameLocation(geoid_a, geoid_b) {
-  // Comparing the strings directly seems sufficient for now, but we might need
-  // to round to fewer decimal places first.
-  return geoid_a == geoid_b;
-}
-
-function makeCaseGraph(geoid) {
-  let svg = d3.select(document.createElementNS(d3.namespaces.svg, 'svg'));
-  svg.attr('width', CASE_GRAPH_WIDTH_PX).
-      attr('height', CASE_GRAPH_HEIGHT_PX);
-
-  let historicalFeaturesForHere = [];
-  let cases = [];
-  for (let i = 0; i < dates.length; i++) {
-    const date = dates[i];
-    let features = atomicFeaturesByDay[date];
-    for (let i = 0; i < features.length; i++) {
-      let f = features[i];
-      if (sameLocation(geoid, f['properties']['geoid'])) {
-        f['properties']['date'] = date;
-        cases.push({
-          'date': d3.timeParse("%Y-%m-%d")(date),
-          'total': f['properties']['total']});
-      }
-    }
-  }
-
-  let xScale = d3.scaleTime()
-      .domain(d3.extent(cases, function(c) { return c['date']; }))
-      .range([0, CASE_GRAPH_WIDTH_PX]);
-
-  svg.append('g')
-      .attr('transform', 'translate(0,' + CASE_GRAPH_HEIGHT_PX + ')')
-      .call(d3.axisBottom(xScale).tickValues([]));
-
-  let yScale = d3.scaleLinear()
-      .domain([0, d3.max(cases, function(c) { return c['total']; })])
-      .range([CASE_GRAPH_HEIGHT_PX, 0]);
-
-  svg.append("g")
-      .call(d3.axisLeft(yScale).tickValues([]));
-
-  let casesLine = d3.line()
-    .x(function(c) { return xScale(c['date']);}) // apply the x scale to the x data
-    .y(function(c) { return yScale(c['total']);}) // apply the y scale to the y data
-
-  svg.append("path")
-      .attr('d', casesLine(cases))
-      .attr('fill', 'none')
-      .attr('stroke', 'steelblue')
-      .attr('stroke-width', 1.5);
-
-  return svg.node();
-}
-
 function showPopupForEvent(e) {
   if (!e['features'].length) {
     // We can't do much without a feature.
@@ -221,7 +163,7 @@ function showPopupForEvent(e) {
       '</h3>' + '<div>' + '<strong>Number of Cases: </strong>' +
       totalCaseCount.toLocaleString() + '</div>';
 
-  content.appendChild(makeCaseGraph(geo_id));
+  content.appendChild(Graphing.makeCaseGraph(geo_id, atomicFeaturesByDay, dates));
 
   // Ensure that if the map is zoomed out such that multiple
   // copies of the feature are visible, the popup appears
